287f561bf94bc58dde155a5040320ed8c3ba1cca
Gian440 <bgiandiri@gmail.com>
2020-03-12 16:22:57 +1100

    12/03 4:22

---
+++ Doodle-Dash-Official-master/Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void ChangeScene(int sceneID)
+    {
+        Debug.Log("Button Pressed");
+        SceneManager.LoadScene(sceneID);
+    }
+    public void ExitToDesktop()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
---
+++ Doodle-Dash-Official-master/Assets/Scripts/VoumeChange.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class VoumeChange : MonoBehaviour
+{
+    //Refers to audio souyrce component
+    private AudioSource audioSrc;
+
+    //Music volume variable that will be modified by dragging slider knob
+    private float musicVolume = 1f;
+
+    //Use this for initialization
+    void Start()
+    {
+        //Assign audio source compent to control it
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        //Setting vloume option of Audio Source to be equal to music version
+        audioSrc.volume = musicVolume;
+    }
+
+    //Method that is called by slider game object
+    //This method takes vol value passed by slider and sets it as musicValue
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
